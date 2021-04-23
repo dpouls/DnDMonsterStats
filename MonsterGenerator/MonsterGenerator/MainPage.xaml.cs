41ec995e3bc1e79f903e4d19ba4f7151faf9dc70
Dallin <dallinpoulson@gmail.com>
2021-04-23 15:54:03 -0600

    first commit

---
+++ MonsterGenerator/MonsterGenerator/MainPage.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace MonsterGenerator
+{
+    public partial class MainPage : ContentPage
+    {
+        Random rand = new Random();
+        
+        public MainPage()
+        {
+            InitializeComponent();
+            LblName.IsVisible = false;
+            LblSize.IsVisible = false;
+            LblType.IsVisible = false;
+            LblAC.IsVisible = false;
+            LblHP.IsVisible = false;
+            LblSpeed.IsVisible = false;
+            LblAlignment.IsVisible = false;
+            LblStats.IsVisible = false;
+            LblStrength.IsVisible = false;
+            LblDex.IsVisible = false;
+            LblCon.IsVisible = false;
+            LblInt.IsVisible = false;
+            LblWis.IsVisible = false;
+            LblChar.IsVisible = false;
+            LblSA.IsVisible = false;
+            LblSADesc.IsVisible = false;
+        }
+        /// <summary>
+        /// we make the call to the API /monsters/ endpoint to get a list of monsters. From there we can get the index of each monster so we make a second API call to /monsters/{index of monster, they're strings not numbers'} using a random index from the array in the first api call.
+        /// 
+        /// From there we change all the label's texts to display the monster's information. 
+        /// We check to make sure there is a walking speed and special abilities before displaying information. 
+        /// We make all the labels visible. 
+        /// 
+        /// It appears the catch method fires if you click the button again too quickly. I'm not sure what's going on there. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnGetMonster_Clicked(object sender, EventArgs e)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                
+                string monsterList = wc.DownloadString($"https://www.dnd5eapi.co/api/monsters/");
+                    JObject parsedMonsterList = JObject.Parse(monsterList);
+                string jsonData = wc.DownloadString($"https://www.dnd5eapi.co/api/monsters/{parsedMonsterList["results"][rand.Next(0,331)]["index"]}/");
+                
+                try
+                {
+                    //JObject parsedJson = JObject.Parse(jsonData);
+                    //LblName.Text = parsedJson["name"].ToString();
+                    //I went with creating a class and deserializing the JSON
+                    MonsterData monsterData = JsonConvert.DeserializeObject<MonsterData>(jsonData);
+                    LblName.Text = $"{monsterData.name}";
+
+                    LblSize.Text = $"Size: \n{monsterData.size.ToString()}";
+                    LblType.Text = $"Type: \n{monsterData.type}";
+                    LblAC.Text = $"Armor Class: \n{monsterData.armor_class.ToString()}";
+                    LblHP.Text = $"HP: \n {monsterData.hit_points.ToString()} or {monsterData.hit_dice}";
+                    if (monsterData.speed.walk != null)
+                    {
+                    LblSpeed.Text = $"Speed: \n{monsterData.speed.walk}";
+                    } else
+                    {
+                        LblSpeed.Text = $"Speed: \n No Data";
+                    }
+                    LblAlignment.Text = $"Alignment: \n{monsterData.alignment}";
+                    LblStrength.Text = $"Strength: \n{monsterData.strength.ToString()}";
+                    LblDex.Text = $"Dexterity: \n{monsterData.dexterity.ToString()}";
+                    LblCon.Text = $"Constitution: \n{monsterData.constitution.ToString()}";
+                    LblInt.Text = $"Intelligence: \n{monsterData.intelligence.ToString()}";
+                    LblWis.Text = $"Wisdom: \n{monsterData.wisdom.ToString()}";
+                    LblChar.Text = $"Charisma: \n{monsterData.charisma.ToString()}";
+                    if (monsterData.special_abilities[0] != null)
+                    {
+                        LblSA.IsVisible = true;
+                        LblSADesc.IsVisible = true;
+                        LblSA.Text = $"Special Ability: {monsterData.special_abilities[0].name}";
+                        LblSADesc.Text = $"{monsterData.special_abilities[0].desc}";
+                    } else
+                    {
+                        LblSA.IsVisible = true;
+                        LblSA.Text = "No Special Abilities.";
+                        LblSADesc.IsVisible = false;
+                    }
+                    LblName.IsVisible = true;
+                    LblSize.IsVisible = true;
+                    LblType.IsVisible = true;
+                    LblAC.IsVisible = true;
+                    LblHP.IsVisible = true;
+                    LblSpeed.IsVisible = true;
+                    LblAlignment.IsVisible = true;
+                    LblStats.IsVisible = true;
+                    LblStrength.IsVisible = true;
+                    LblDex.IsVisible = true;
+                    LblCon.IsVisible = true;
+                    LblInt.IsVisible = true;
+                    LblWis.IsVisible = true;
+                    LblChar.IsVisible = true;
+                    
+                } 
+                catch (Exception ex)
+                {
+                    DisplayAlert("Oh no!",$"Some goblins must have sabotaged the server! \n ({ex.Message})", "Close");
+                }
+            }
+        }
+    }
+}
---
+++ MonsterGenerator/MonsterGenerator/MonsterData.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonsterGenerator
+{
+
+    public class MonsterData
+    {
+        public string index { get; set; }
+        public string name { get; set; }
+        public string size { get; set; }
+        public string type { get; set; }
+        public string subtype { get; set; }
+        public string alignment { get; set; }
+        public int armor_class { get; set; }
+        public int hit_points { get; set; }
+        public string hit_dice { get; set; }
+        public Speed speed { get; set; }
+        public int strength { get; set; }
+        public int dexterity { get; set; }
+        public int constitution { get; set; }
+        public int intelligence { get; set; }
+        public int wisdom { get; set; }
+        public int charisma { get; set; }
+        public Proficiency[] proficiencies { get; set; }
+        public object[] damage_vulnerabilities { get; set; }
+        public object[] damage_resistances { get; set; }
+        public object[] damage_immunities { get; set; }
+        public object[] condition_immunities { get; set; }
+        public Senses senses { get; set; }
+        public string languages { get; set; }
+        public float challenge_rating { get; set; }
+        public int xp { get; set; }
+        public Special_Abilities[] special_abilities { get; set; }
+        public Action[] actions { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Speed
+    {
+        public string walk { get; set; }
+    }
+
+    public class Senses
+    {
+        public string darkvision { get; set; }
+        public int passive_perception { get; set; }
+    }
+
+    public class Proficiency
+    {
+        public int value { get; set; }
+        public Proficiency1 proficiency { get; set; }
+    }
+
+    public class Proficiency1
+    {
+        public string index { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Special_Abilities
+    {
+        public string name { get; set; }
+        public string desc { get; set; }
+    }
+
+    public class Action
+    {
+        public string name { get; set; }
+        public string desc { get; set; }
+        public int attack_bonus { get; set; }
+        public Damage[] damage { get; set; }
+    }
+
+    public class Damage
+    {
+        public Damage_Type damage_type { get; set; }
+        public string damage_dice { get; set; }
+    }
+
+    public class Damage_Type
+    {
+        public string index { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+
+}
